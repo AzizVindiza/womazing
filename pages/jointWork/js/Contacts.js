d5e1f12a2a44c771d44b4d3092efe85be40f85f9
Your Name <ihiya.sultan@mail.ru>
2022-11-29 21:25:16 +0600

    join

---
+++ pages/jointWork/js/Contacts.js
@@ -0,0 +1,5 @@
+let submit = document.querySelector(".writeToUs__submit")
+let answer = document.querySelector(".writeToUs__resolve")
+submit.addEventListener("click",()=>{
+    answer.style.display = "flex"
+})
